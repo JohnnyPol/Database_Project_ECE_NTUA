528401c16fb670250343e02ef43bc4ffa941df0b
john <46429733+JohnnyPol@users.noreply.github.com>
2024-05-08 16:07:01 +0300

    Create Syntax_file.sql

---
+++ Syntax_file.sql
@@ -0,0 +1,20 @@
+-- Create a user with a specific password
+CREATE USER 'limiteduser'@'localhost' IDENTIFIED BY 'password';
+
+-- Grant the EXECUTE privilege on the function to the user.
+GRANT EXECUTE ON FUNCTION HelloUser TO 'limiteduser'@'localhost';
+
+--Grant All Privileges on a Specific Database:
+GRANT ALL PRIVILEGES ON databasename.* TO 'newusername'@'localhost';
+
+-- Grant Only Insert and Select Privileges:
+GRANT INSERT, SELECT ON databasename.* TO 'newusername'@'localhost';
+
+-- Revoke a Privilege:
+REVOKE INSERT ON databasename.* FROM 'newusername'@'localhost';
+
+-- Apply the privilages
+FLUSH PRIVILEGES;
+
+--Delete a User:
+DROP USER 'newusername'@'localhost';
