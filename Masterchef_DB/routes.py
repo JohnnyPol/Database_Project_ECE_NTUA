8a4bd1f2d04fbed5bd14260ea479d75f4f4ba61d
john <giannis.polychronopoulos@gmail.com>
2024-05-18 20:12:28 +0300

    modified:   Masterchef_DB/routes.py

--- Masterchef_DB/routes.py
+++ Masterchef_DB/routes.py
@@ -7,16 +7,19 @@ def index():
     try:
         # Attempt to execute a simple query to test the database connection
         cursor = db.connection.cursor()
-        cursor.execute("SELECT * FROM recipes")
+        query = """
+        SELECT prep_time FROM masterchef_ntua_edition.recipes WHERE recipe_name="Apple Pie";
+"""
+        cursor.execute(query)
         
         column_names = [i[0] for i in cursor.description]
         # Fetch all rows from the result set
-        results = dict(zip(column_names, cursor.fetchone()))
+        results = cursor.fetchone()
 
         cursor.close()
         
         # Return the processed results as JSON
-        return jsonify(results)
+        return jsonify(str(results))
     except Exception as e:
         return jsonify(message="Error connecting to database: {}".format(str(e))), 500
 
