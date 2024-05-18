5e895ea46d54477b07a93d0f8db13b8e1e641505
john <giannis.polychronopoulos@gmail.com>
2024-05-18 19:13:34 +0300

    modified:   .gitignore 	modified:   Masterchef_DB/__init__.py 	modified:   Masterchef_DB/config.json 	new file:   Masterchef_DB/routes.py

--- Masterchef_DB/__init__.py
+++ Masterchef_DB/__init__.py
@@ -1,7 +1,9 @@
 ## This file is ran automatically the first time a Python program imports the package dbdemo
 from flask import Flask
 from flask_mysqldb import MySQL
+import json
 
 ## Create an instance of the Flask class to be used for request routing
 app = Flask(__name__)
 
+app.config.from_file("config.json", load=json.load)
