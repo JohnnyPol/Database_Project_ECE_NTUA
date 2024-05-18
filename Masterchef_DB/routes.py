d0285c8488bc44a5e903794dcdbe84c9bab2c1ba
john <giannis.polychronopoulos@gmail.com>
2024-05-18 19:49:28 +0300

    modified:   Masterchef_DB/config.json 	modified:   Masterchef_DB/routes.py

--- Masterchef_DB/routes.py
+++ Masterchef_DB/routes.py
@@ -1,16 +1,22 @@
-from flask import Flask, request, jsonify
-from flask_mysqldb import MySQLdb
+from flask import jsonify
 from Masterchef_DB import app, db
-
+from datetime import timedelta
 
 @app.route("/")
 def index():
     try:
         # Attempt to execute a simple query to test the database connection
         cursor = db.connection.cursor()
-        cursor.execute("SELECT 1")
-        result = cursor.fetchone()
+        cursor.execute("SELECT * FROM recipes")
+        
+        column_names = [i[0] for i in cursor.description]
+        # Fetch all rows from the result set
+        results = dict(zip(column_names, cursor.fetchone()))
+
         cursor.close()
-        return jsonify(message="Database connection successful"), 200
+        
+        # Return the processed results as JSON
+        return jsonify(results)
     except Exception as e:
         return jsonify(message="Error connecting to database: {}".format(str(e))), 500
+
